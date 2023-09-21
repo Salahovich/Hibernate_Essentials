df798e4af05591aa86563b839095be2029c5f20e
Muhammad Salah Abdallah <84870808+Salahovich@users.noreply.github.com>
2023-09-21 15:00:18 +0300

    Init

---
+++ src/main/java/example/Student.java
@@ -0,0 +1,117 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Student {
+    public enum Gender {MALE, FEMALE}
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
+    private String email;
+    private String phoneNumber;
+    private String nationalId;
+    public Student(){}
+
+    public Student(UUID id, String firstName, String lastName, int age, Gender gender, String email, String phoneNumber, String nationalId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.nationalId = nationalId;
+    }
+
+    public Student(String firstName, String lastName, int age, Gender gender, String email, String phoneNumber, String nationalId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.nationalId = nationalId;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String national_id) {
+        this.nationalId = national_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", nationalId='" + nationalId + '\'' +
+                '}' + '\n';
+    }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,30 @@
+package $org.example;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+
+    /**
+     * Rigourous com.Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
